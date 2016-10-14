0357145de33730fc13e92f7f01973aeb53fcb7e2
Holden Easley <holden@interapthq.com>
2016-10-14 11:32:35 -0400

    Added Picasso to load images

--- MyApplication/app/src/main/java/com/example/interapt/libraryexamples/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/interapt/libraryexamples/MainActivity.java
@@ -3,28 +3,29 @@ package com.example.interapt.libraryexamples;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.ImageView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-  // Binds the variable editText with type off EditText to the view with id of R.id.edit_text
+  @BindView(R.id.image_view) protected ImageView imageView;
+
   @BindView(R.id.edit_text) protected EditText editText;
 
-  // Use on @OnClick annotation to denote to call the method below when
-  // the view referenced inside (R.id.button) is clicked
   @OnClick(R.id.button) protected void onButtonClicked() {
-    if (getSupportActionBar() != null) {
-      getSupportActionBar().setTitle(editText.getText().toString());
-    }
+    // fetch a random image and load it into the imageView using Picasso
+    Picasso.with(this)
+        .load("http://loremflickr.com/320/240/" + editText.getText().toString())
+        .into(imageView);
   }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    // Can remove all the old code and replace it with
     ButterKnife.bind(this);
   }
 }
