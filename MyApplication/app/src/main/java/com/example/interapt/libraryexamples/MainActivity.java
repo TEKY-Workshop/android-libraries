83ed8e64628df9d43135cf382bb52c5239d47c76
Holden Easley <holden@interapthq.com>
2016-10-14 11:01:52 -0400

    Added Butterknife

--- MyApplication/app/src/main/java/com/example/interapt/libraryexamples/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/interapt/libraryexamples/MainActivity.java
@@ -1,38 +1,30 @@
 package com.example.interapt.libraryexamples;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+  // Binds the variable editText with type off EditText to the view with id of R.id.edit_text
+  @BindView(R.id.edit_text) protected EditText editText;
+
+  // Use on @OnClick annotation to denote to call the method below when
+  // the view referenced inside (R.id.button) is clicked
+  @OnClick(R.id.button) protected void onButtonClicked() {
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().setTitle(editText.getText().toString());
+    }
+  }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    // get reference to button, editText, and actionbar
-    Button button = (Button) findViewById(R.id.button);
-
-    // must be final because it is referenced in the on click method
-    // and it is not a member variable
-    final EditText editText = (EditText) findViewById(R.id.edit_text);
-
-    // method from AppCompatActivity to get the current actionbar
-    // again final for the same reasons as above
-    final ActionBar actionBar = getSupportActionBar();
-
-    // Set on click listener
-    button.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        if (actionBar != null) {
-          // call to string to strip any styling the Editable has
-          actionBar.setTitle(editText.getText().toString());
-        }
-      }
-    });
+    // Can remove all the old code and replace it with
+    ButterKnife.bind(this);
   }
 }
