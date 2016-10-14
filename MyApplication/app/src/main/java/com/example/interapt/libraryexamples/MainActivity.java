d8e3ad3c43602f25b629eac2f0a2eeaa9fe34dd2
Holden Easley <holden@interapthq.com>
2016-10-14 11:41:09 -0400

    Added material dialog to improve UX

--- MyApplication/app/src/main/java/com/example/interapt/libraryexamples/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/interapt/libraryexamples/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.ImageView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,10 +18,34 @@ public class MainActivity extends AppCompatActivity {
   @BindView(R.id.edit_text) protected EditText editText;
 
   @OnClick(R.id.button) protected void onButtonClicked() {
+    // create material dialog and show it
+    final MaterialDialog dialog = new MaterialDialog.Builder(this).progress(true, 0)
+        .title(R.string.image_loading_text)
+        .show();
+
     // fetch a random image and load it into the imageView using Picasso
     Picasso.with(this)
         .load("http://loremflickr.com/320/240/" + editText.getText().toString())
-        .into(imageView);
+        .into(imageView, new Callback() {
+          @Override public void onSuccess() {
+            // dismiss dialog on success
+            dialog.dismiss();
+          }
+
+          @Override public void onError() {
+            // dismiss dialog on error
+            dialog.dismiss();
+
+            // show error dialog, no need to create a variable to hold reference to it
+            new MaterialDialog.Builder(MainActivity.this).title(R.string.error_loading_image_title)
+                .content(R.string.error_loading_image_content)
+                .positiveText(R.string.ok)
+                .show();
+
+            // or toast, whichever fits your UX better
+            //Toast.makeText(MainActivity.this, R.string.error_loading_image_title,Toast.LENGTH_LONG).show();
+          }
+        });
   }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
